a211be8030419cf8ee7034692820eb8c28cf0abd
Danial Ayub Mughal <danialayubmughal28@gmail.com>
2025-05-14 18:49:09 +0500

    Add files via upload

---
+++ Lab 02.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include"Employee.h"
+#include"Bank.h"
+#include"Rectangle.h"
+#include"Locker.h"
+using namespace std;
+int main(){
+//cout<<"============================01======================"<<endl;
+//	Employee E;
+//	E.displayDetail();
+//	return 0;
+//cout<<"===========================02======================="<<endl;
+//	BankAccount b("PK0309800780100","Muhammad Awais",10000);
+//	b.showAccountDetail();
+//cout<<"============================03======================"<<endl;
+//    Rectangle r1;
+//	Rectangle r2(4.0,5.0);
+//	Rectangle r3(6.0);
+//	r1.display();
+//	r2.display();
+//	r3.display();
+cout<<"============================04======================"<<endl;
+{
+Locker locker1;
+}
+Locker*locker2=new Locker();
+delete locker2;
+return 0;
+}
+
